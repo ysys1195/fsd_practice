bc804ab4fd9905d0cca9932b4e69ee3f82940d7a
kasumi-yoshida-pasona <kasyoshida@fab.pasona.tech>
2024-08-08 18:46:09 +0900

    scssの修正

--- src/app/App.vue
+++ src/app/App.vue
@@ -2,15 +2,8 @@
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank"> </a>
-
     <router-view></router-view>
   </div>
 </template>
 
-<style scoped>
-@import "./index.scss";
-</style>
+<style></style>
