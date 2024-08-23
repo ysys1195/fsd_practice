94c76e18738866020b4c441b1d87faa39d0ae4e8
kasumi-yoshida-pasona <kasyoshida@fab.pasona.tech>
2024-08-23 11:33:01 +0900

    eslintでFSDを強制

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    eslintPlugin({
+      // オプションでESLintの設定をカスタマイズできます
+      cache: false, // ESLintのキャッシュを無効にする例
+    }),
+  ],
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
