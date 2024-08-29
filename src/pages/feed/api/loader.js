bf30d6881d8a70430c36b0466c8b24cde5ccd1ce
kasumi-yoshida-pasona <kasyoshida@fab.pasona.tech>
2024-08-29 15:59:02 +0900

    Pagination

--- src/pages/feed/api/loader.js
+++ src/pages/feed/api/loader.js
@@ -1,19 +1,32 @@
 import { GET } from "@/shared/api";
 
 export const loader = async ({ request }) => {
+  const LIMIT = 20;
   const url = new URL(request.url);
   const selectedTag = url.searchParams.get("tag") || undefined;
+  const page = parseInt(url.searchParams.get("page") || "", 10);
+
   try {
     const [articlesResponse, tagsResponse] = await Promise.all([
-      GET("/articles", { params: { query: { tag: selectedTag } } }),
+      GET("/articles", {
+        params: {
+          query: {
+            tag: selectedTag,
+            limit: LIMIT,
+            offset: !Number.isNaN(page) ? page * LIMIT : undefined,
+          },
+        },
+      }),
       GET("/tags"),
     ]);
 
-    const articles = articlesResponse.data.articles;
+    const articles = articlesResponse.data;
     const tags = tagsResponse.data.tags;
 
     return { articles, tags };
   } catch (error) {
+    console.log(error);
+
     throw { error: error.response.data, status: error.response.status };
   }
 };
--- src/pages/feed/ui/FeedPage.vue
+++ src/pages/feed/ui/FeedPage.vue
@@ -1,22 +1,42 @@
 <script setup>
 import { ref, onMounted } from "vue";
-// import ArticlePreview from "./ArticlePreview.vue";
+import { useRoute, useRouter } from "vue-router";
+import ArticlePreview from "./ArticlePreview.vue";
 import feedModule from "../index";
 const { loader } = feedModule;
 
-const articlesInfo = ref([]); // articlesをrefで初期化
-const tagsInfo = ref([]); // articlesをrefで初期化
+const route = useRoute();
+const router = useRouter();
+const articlesInfo = ref([]);
+const tagsInfo = ref([]);
+const pageAmount = ref([]);
+const currentPage = ref([]);
+const LIMIT = 20;
 
 onMounted(async () => {
   try {
     // loader関数を実行し、articlesに結果を格納
-    const { articles, tags } = await loader();
-    articlesInfo.value = articles; // articlesプロパティにアクセス
+    const { articles, tags } = await loader({
+      request: new Request(route.fullPath),
+    });
+
+    articlesInfo.value = articles.articles; // articlesプロパティにアクセス
     tagsInfo.value = tags;
+
+    pageAmount.value = Math.ceil(articles.articlesCount / LIMIT);
+    currentPage.value = parseInt(route.query.page || "1", 10);
   } catch (error) {
     console.error("Error loading articles:", error);
   }
 });
+
+const setPage = (page) => {
+  router.push({ query: { ...route.query, page } });
+};
+
+const setTag = (tag) => {
+  router.push({ query: { ...route.query, tag, page: 1 } });
+};
 </script>
 
 <template>
@@ -34,23 +54,43 @@ onMounted(async () => {
             :key="article.slug"
             :article="article"
           />
+
+          <form>
+            <ul class="pagination">
+              <li
+                v-for="page in pageAmount"
+                :key="page"
+                :class="['page-item', { active: page === currentPage }]"
+              >
+                <button
+                  v-if="page !== currentPage"
+                  class="page-link"
+                  @click.prevent="setPage(page)"
+                >
+                  {{ page }}
+                </button>
+                <span v-else class="page-link">{{ page }}</span>
+              </li>
+            </ul>
+          </form>
         </div>
 
         <div class="col-md-3">
           <div class="sidebar">
             <p>Popular Tags</p>
 
-            <Form>
+            <form>
               <div class="tag-list">
                 <button
                   v-for="tag in tagsInfo"
                   :key="tag"
                   class="tag-pill tag-default"
+                  @click.prevent="setTag(tag)"
                 >
                   {{ tag }}
                 </button>
               </div>
-            </Form>
+            </form>
           </div>
         </div>
       </div>
