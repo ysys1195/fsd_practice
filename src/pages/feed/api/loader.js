f04502aca2ef613ae3cd1cc81dfc9fbb237a55b8
kasumi-yoshida-pasona <kasyoshida@fab.pasona.tech>
2024-08-29 13:09:02 +0900

    tagのFiltering

--- src/pages/feed/api/loader.js
+++ src/pages/feed/api/loader.js
@@ -1,11 +1,19 @@
 import { GET } from "@/shared/api";
 
-export const loader = async () => {
-  const { data: articles, error, response } = await GET("/articles");
+export const loader = async ({ request }) => {
+  const url = new URL(request.url);
+  const selectedTag = url.searchParams.get("tag") || undefined;
+  try {
+    const [articlesResponse, tagsResponse] = await Promise.all([
+      GET("/articles", { params: { query: { tag: selectedTag } } }),
+      GET("/tags"),
+    ]);
 
-  if (error !== undefined) {
-    throw { error, status: response.status };
+    const articles = articlesResponse.data.articles;
+    const tags = tagsResponse.data.tags;
+
+    return { articles, tags };
+  } catch (error) {
+    throw { error: error.response.data, status: error.response.status };
   }
-
-  return articles;
 };
--- src/pages/feed/ui/FeedPage.vue
+++ src/pages/feed/ui/FeedPage.vue
@@ -1,16 +1,18 @@
 <script setup>
 import { ref, onMounted } from "vue";
-import ArticlePreview from "./ArticlePreview.vue";
+// import ArticlePreview from "./ArticlePreview.vue";
 import feedModule from "../index";
 const { loader } = feedModule;
 
-const articles = ref([]); // articlesをrefで初期化
+const articlesInfo = ref([]); // articlesをrefで初期化
+const tagsInfo = ref([]); // articlesをrefで初期化
 
 onMounted(async () => {
   try {
     // loader関数を実行し、articlesに結果を格納
-    const result = await loader();
-    articles.value = result.articles; // articlesプロパティにアクセス
+    const { articles, tags } = await loader();
+    articlesInfo.value = articles; // articlesプロパティにアクセス
+    tagsInfo.value = tags;
   } catch (error) {
     console.error("Error loading articles:", error);
   }
@@ -24,14 +26,32 @@ onMounted(async () => {
       <p>A place to share your knowledge.</p>
     </div>
 
-    <div v-if="articles.length > 0" class="container page">
+    <div v-if="articlesInfo.length > 0" class="container page">
       <div class="row">
         <div class="col-md-9">
           <ArticlePreview
-            v-for="article in articles"
+            v-for="article in articlesInfo"
             :key="article.slug"
             :article="article"
           />
+        </div>
+
+        <div class="col-md-3">
+          <div class="sidebar">
+            <p>Popular Tags</p>
+
+            <Form>
+              <div class="tag-list">
+                <button
+                  v-for="tag in tagsInfo"
+                  :key="tag"
+                  class="tag-pill tag-default"
+                >
+                  {{ tag }}
+                </button>
+              </div>
+            </Form>
+          </div>
         </div>
       </div>
     </div>
